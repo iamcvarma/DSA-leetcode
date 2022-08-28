03544818b9fefb59e1047b0daceb28b322de8559
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-08-28 19:26:00 +0530

    Time: 832 ms (70.75%), Space: 15.2 MB (50.72%)

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,14 +1,21 @@
+#A* path finding algorigthm
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0]:return -1
         n=len(grid)
-        q=deque([(0,0,1)])
-        grid[0][0]=1
-        while q:
-            i,j,d=q.popleft()
-            if i==n-1 and j==n-1:return d
+        target=(n-1,n-1)
+        score={(0,0):2*n}
+        heap=[(2*n,1,0,0)]
+        while heap:
+            f,g,i,j=heapq.heappop(heap)
+            if (i,j)==target:return g
             for x,y in ((i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j+1),(i+1,j-1),(i-1,j+1),(i-1,j-1)):
                 if x<0 or y<0 or x>=n or y>=n or grid[x][y]:continue
-                grid[x][y]=1
-                q.append((x,y,d+1))
+                heu=max(abs(x-n),abs(y-n))
+                f=heu+g+1
+                key=(x,y)
+                if key not in score or score[key]>f:
+                    score[key]=f
+                    heapq.heappush(heap,(f,g+1,x,y))
         return -1
