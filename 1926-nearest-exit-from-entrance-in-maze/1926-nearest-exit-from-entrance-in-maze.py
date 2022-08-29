0c58d8277d1cdcf23eadbbfcfa1827bfbbbcfc75
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-08-29 19:41:46 +0530

    Time: 1145 ms (60.42%), Space: 14.5 MB (87.64%)

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,26 +1,17 @@
 class Solution:
     def nearestExit(self, grid: List[List[str]], ent: List[int]) -> int:
         m,n=len(grid),len(grid[0])
-        q=deque([])
-        grid[ent[0]][ent[1]]='*'
-        for i in range(m):
-            if grid[i][0]=='.':
-                q.append((i,0))
-            if grid[i][n-1]=='.':
-                q.append((i,n-1))
-        for j in range(n):
-            if grid[0][j]=='.':
-                q.append((0,j))
-            if grid[m-1][j]=='.':
-                q.append((m-1,j))
+        q=[(ent[0],ent[1])]
+        grid[ent[0]][ent[1]]='+'
         steps=0
         while q:
-            for _ in range(len(q)):
-                i,j=q.popleft()
-                if ent==[i,j]:return steps
+            new=[]
+            for i,j in q:
+                if (i==0 or i==m-1 or j==0 or j==n-1) and [i,j]!=ent:return steps
                 for x,y in ((i+1,j),(i-1,j),(i,j+1),(i,j-1)):
                     if -1<x<m and -1<y<n and grid[x][y]!='+':
-                        q.append((x,y))
+                        new.append((x,y))
                         grid[x][y]='+'
             steps+=1
+            q=new
         return -1
