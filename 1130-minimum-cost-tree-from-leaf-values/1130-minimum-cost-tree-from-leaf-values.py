d962f7cbf93b35b42338851c5610db501911ed76
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-08-30 17:57:55 +0530

    Time: 51 ms (67.67%), Space: 13.7 MB (98.50%)

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
@@ -1,15 +1,13 @@
 class Solution:
     def mctFromLeafValues(self, arr: List[int]) -> int:
-        def solve(i,j):
-            if i>=j:return 0
-            if (i,j) in dp:return dp[(i,j)]
-            ans=inf
-            for k in range(i,j):
-                left=solve(i,k)
-                right=solve(k+1,j)
-                ans=min(ans,left+right+(max(arr[i:k+1])*max(arr[k+1:j+1])))
-            dp[(i,j)]=ans
-            return ans
-        dp={}
-        return solve(0,len(arr)-1)
-                
+        st,res=[inf],0
+        
+        for n in arr:
+            while st and st[-1]<=n:
+                curr=st.pop()
+                if st: res+=curr*min(st[-1],n)
+            st.append(n)
+            
+        while len(st)>2:
+            res+= st.pop()*st[-1]
+        return res
