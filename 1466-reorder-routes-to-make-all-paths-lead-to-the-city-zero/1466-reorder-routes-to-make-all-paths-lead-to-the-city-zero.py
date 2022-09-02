857cb4d919a61760e4a04a3370ae13379b3bda42
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-02 13:24:19 +0530

    Time: 2241 ms (27.40%), Space: 70.3 MB (49.93%)

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,21 +1,15 @@
 class Solution:
-    res=0
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         def dfs(node,pre):
-            if node in ori[pre]:
-                self.res+=1
+            res[0]+= 1 if (pre,node) in edges else 0
             for child in adj[node]:
-                if child!=pre:
-                    dfs(child,node)
-            return
+                if child!=pre:dfs(child,node)
         
-        
+        res=[0]
         adj=defaultdict(list)
-        ori=defaultdict(list)
-        
+        edges={(x,y) for x,y in connections}
         for x, y in connections:
-            ori[x].append(y)
             adj[x].append(y)
             adj[y].append(x)
         dfs(0,-1)
-        return self.res
+        return res[0]
