181705eeaf8e152c9a7db037c244cf37d2f7a3eb
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-03 19:58:58 +0530

    Time: 379 ms (39.21%), Space: 33.9 MB (37.22%)

---
+++ 365-water-and-jug-problem/365-water-and-jug-problem.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canMeasureWater(self,a: int, b: int, z: int) -> bool:
+        if a+b<z:return False
+        if a<b:a,b=b,a
+        arr=[a,b,-b]
+        seen=set([0])
+        q=deque([0])
+        while q:
+            curr=q.pop()
+            for n in arr:
+                val=n+curr
+                if val==z:return True
+                if 0<=val<=a+b and val not in seen:
+                    seen.add(val)
+                    q.append(val)
+        return False
