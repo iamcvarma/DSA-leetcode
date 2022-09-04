49a2ff77f0505f97b76fddaf6fd49b829228a3c1
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-04 13:36:01 +0530

    Time: 250 ms (97.15%), Space: 14.6 MB (97.86%)

--- 752-open-the-lock/752-open-the-lock.py
+++ 752-open-the-lock/752-open-the-lock.py
@@ -1,22 +1,26 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         deadends=set(deadends)
-        q=deque(["0000"])
-        if "0000" in deadends:return -1
-        deadends.add("0000")
+        start="0000"
+        if target==start:return 0
+        if start in deadends:return -1
+        q1,q2=set([start]),set([target])
+        deadends.add(start)
+        deadends.add(target)
         dist=0
-        while q:
-            for _ in range(len(q)):
-                curr=q.popleft()
-                if curr==target:return dist
+        while q1:
+            if len(q1)>len(q2):
+                q1,q2=q2,q1
+            new=set()
+            for curr in q1:
                 for i in range(4):
-                    new=curr[:i]+str((10+int(curr[i])+1)%10)+curr[i+1:]
-                    if new not in deadends:
-                        q.append(new)
-                        deadends.add(new)
-                    new=curr[:i]+str((10+int(curr[i])-1)%10)+curr[i+1:]
-                    if new not in deadends:
-                        q.append(new)
-                        deadends.add(new)
+                    num=int(curr[i])
+                    for c in ((num+1)%10,(10+num-1)%10):
+                        code=curr[:i]+str(c)+curr[i+1:]
+                        if code in q2:return dist+1
+                        if code not in deadends:
+                            deadends.add(code)
+                            new.add(code)
+            q1=new
             dist+=1
         return -1
