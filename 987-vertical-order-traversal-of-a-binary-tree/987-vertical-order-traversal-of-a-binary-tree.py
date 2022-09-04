5514775095361822f1de072a39835e1c1c200f67
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-04 11:55:08 +0530

    Time: 76 ms (6.94%), Space: 14.1 MB (72.74%)

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -7,14 +7,17 @@
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         hmap=defaultdict(list)
+        mi,ma=inf,-inf
         def tra(root,i,j):
+            nonlocal mi,ma
             if not root:return
+            mi=min(mi,j)
+            ma=max(ma,j)
             hmap[j].append((i,root.val))
             tra(root.left,i+1,j-1)
             tra(root.right,i+1,j+1)
         tra(root,0,0)
         res=[]
-        mi,ma=min(hmap),max(hmap)
         for i in range(mi,ma+1):
             res.append([y for x,y in sorted(hmap[i])])
         return res
