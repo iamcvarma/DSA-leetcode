ecdf135a4d2cffa1a38b55f0464f050d21767be7
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-04 14:01:31 +0530

    Time: 238 ms (62.88%), Space: 15.1 MB (70.83%)

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,10 +1,9 @@
 class Solution:
     def ladderLength(self, start: str, end: str, wordList: List[str]) -> int:
         words=set(wordList)
-        if start==end:return 0
-        if end not in words:return 0
+        if start==end or end not in words:return 0
         q1,q2=set([start]),set([end])
-        seen=set([start,end])
+        words.add(start)
         dist=1
         while q1:
             if len(q1)>len(q2):
@@ -15,9 +14,9 @@ class Solution:
                     for c in "abcdefghijklmnopqrstuvwxyz":
                         word=curr[:i]+c+curr[i+1:]
                         if word in q2:return dist+1
-                        if word in words and word not in seen:
+                        if word in words:
                             new.add(word)
-                            seen.add(word)
+                            words.remove(word)
             dist+=1
             q1=new
         return 0
