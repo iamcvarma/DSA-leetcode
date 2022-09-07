b175ea867749dbb44dd50b37ab1de1336d2cd9c3
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-07 12:16:26 +0530

    Time: 84 ms (49.63%), Space: 16.5 MB (30.02%)

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if not root : return ""
+        left = self.tree2str(root.left)
+        right = self.tree2str(root.right)
+        ans = str(root.val)
+        if left!="" or right!="":
+            ans+="("+left+")"
+        if right!="":ans+="("+right+")"
+        return ans
