c59a8033e7d7cdfcf4c378777d7fc4de0f4028f8
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-08 18:04:31 +0530

    Time: 1848 ms (5.00%), Space: 0B (100.00%)

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -1,5 +1,5 @@
 # Please write a DELETE statement and DO NOT write a SELECT statement.
 # Write your MySQL query statement below
 DELETE p1 
-From Person p1,Person p2 
+From Person p1 CROSS JOIN Person p2
 WHERE p1.email=p2.email and p1.id>p2.id
