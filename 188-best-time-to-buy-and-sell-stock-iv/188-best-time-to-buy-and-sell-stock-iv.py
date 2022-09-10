4cb9fa28724d70d6361f3742e0ff0c82cad326f4
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-10 16:24:21 +0530

    Time: 219 ms (48.27%), Space: 14 MB (79.28%)

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,9 +1,7 @@
 class Solution:
-    def maxProfit(self, k: int, prices: List[int]) -> int:
-        n=len(prices)
-        dp=[[[0,0] for _ in range(k+1)] for __ in range(n+1)]
+    def maxProfit(self, cap: int, prices: List[int]) -> int:
+        n,buy,sell=len(prices), [0]*(cap+1), [0]*(cap+1)
         for i in range(n-1,-1,-1):
-            for cap in range(1,k+1):
-                dp[i][cap][1]=max(-prices[i]+dp[i+1][cap][0],dp[i+1][cap][1])
-                dp[i][cap][0]=max(prices[i]+dp[i+1][cap-1][1],dp[i+1][cap][0])
-        return dp[0][k][1]
+            for k in range(1,cap+1):
+                buy[k],sell[k]=max(-prices[i]+sell[k],buy[k]),max(prices[i]+buy[k-1],sell[k])
+        return buy[cap]
