db40edb3e41a9180e065dabb6fe419814b59d78f
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-10 16:33:09 +0530

    Time: 94 ms (20.10%), Space: 14.5 MB (54.81%)

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,13 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        def solve(i,buy):
-            if i>=len(prices):return 0
-            k=(i,buy)
-            if k in dp:return dp[k]
-            if buy:
-                dp[k]=max(-prices[i]+solve(i+1,0),solve(i+1,1))
-            else:
-                dp[k]=max(prices[i]+solve(i+2,1),solve(i+1,0))
-            return dp[k]
-        dp={}
-        return solve(0,1)
+        n=len(prices)
+        dp=[[0,0] for _ in range(n+2)]
+        for i in range(n-1,-1,-1):
+            dp[i][1]=max(-prices[i]+dp[i+1][0],dp[i+1][1])
+            dp[i][0]=max(prices[i]+dp[i+2][1],dp[i+1][0])
+        return dp[0][1]
