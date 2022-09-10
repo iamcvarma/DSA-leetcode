8eb4ca8cd94cc29917b90d10482cc28e854ba6b6
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-10 13:36:47 +0530

    Time: 182 ms (5.09%), Space: 17.2 MB (6.62%)

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,14 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        def solve(i,buy):
-            if i==n:return 0
-            key = (i,buy)
-            if key in dp:return dp[key]
-            if buy:
-                dp[key]=max(-prices[i]+solve(i+1,0),solve(i+1,1))
-            else:
-                dp[key]= max(prices[i]+solve(i+1,1),solve(i+1,0))
-            return dp[key]
         n=len(prices)
-        dp={}
-        return solve(0,1)
+        dp=[[0,0] for _ in range(n)]
+        dp[0]=[-prices[0],0]
+        for i in range(1,n):
+            dp[i][0]=max(-prices[i]+dp[i-1][1],dp[i-1][0])
+            dp[i][1]=max(prices[i]+dp[i-1][0],dp[i-1][1])
+        return dp[-1][1]
