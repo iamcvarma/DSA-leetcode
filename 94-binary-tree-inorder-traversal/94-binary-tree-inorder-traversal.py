7be753602feb3976f2d7fd81c019e4455f19101a
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-11 19:53:39 +0530

    Time: 72 ms (5.21%), Space: 13.9 MB (59.78%)

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -1,18 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        #iterative
-        st,res,curr=[],[],root
-        while st or curr:
-            while curr:
-                st.append(curr)
-                curr=curr.left
-            root=st.pop()
-            res.append(root.val)
-            curr=root.right
+        #morris Traversal
+        res=[]
+        while root:
+            if root.left==None:
+                res.append(root.val)
+                root = root.right
+            else:
+                ptr = root.left
+                while ptr.right and ptr.right!=root:
+                    ptr = ptr.right
+                if not ptr.right:
+                    ptr.right = root
+                    root = root.left
+                else:
+                    ptr.right = None
+                    res.append(root.val)
+                    root = root.right
         return res
