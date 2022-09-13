4523fe461d37469c5afa995051913f51d5bb7f23
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-13 12:24:11 +0530

    Time: 270 ms (91.82%), Space: 15.4 MB (32.29%)

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.py
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.py
@@ -1,10 +1,5 @@
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
-        mi = nums[0]
-        res = 0
-        for i,v in enumerate(nums):
-            if v<mi:
-                res+=i*(mi-v)
-                mi=v
-            res+=v-mi
-        return res
+        mi = min(nums)
+        return sum([n-mi for n in nums])
+        
