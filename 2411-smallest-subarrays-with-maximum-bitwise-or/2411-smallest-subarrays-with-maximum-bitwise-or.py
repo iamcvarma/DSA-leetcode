73bdca63fdaafe12b5b626c77af3ce7661b0fe17
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-09-20 18:06:52 +0530

    Time: 2867 ms (84.65%), Space: 27.7 MB (96.85%)

--- 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.py
+++ 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.py
@@ -1,15 +1,11 @@
 class Solution:
     def smallestSubarrays(self, nums: List[int]) -> List[int]:
-        arr =[inf]*32
+        arr =[0]*32
         for j in range(len(nums)-1,-1,-1):
             i=0
             while (1<<i)<=nums[j]:
                 if (1<<i)&nums[j]:
                     arr[i] = j
                 i+=1
-            temp = 0
-            for n in arr:
-                if n<inf:temp = max(temp,n)
-            nums[j] = (temp or j)-j+1
-            
+            nums[j] = max(max(arr)-j+1,1)
         return nums
