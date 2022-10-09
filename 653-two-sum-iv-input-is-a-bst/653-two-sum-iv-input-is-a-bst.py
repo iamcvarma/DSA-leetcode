a25449096e44c77797d8b9a77c6402a5065a240a
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-10-09 23:38:47 +0530

    Time: 143 ms (61.17%), Space: 16.3 MB (95.44%)

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -1,22 +1,41 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class LeftIter:
+    def __init__(self,root):
+        self.st=[]
+        self.add(root)
+    
+    def add(self,root):
+        while root:
+            self.st.append(root)
+            root=root.left
+    def nxt(self):
+        res=self.st.pop()
+        self.add(res.right)
+        return res.val
+    
+class RightIter:
+    def __init__(self,root):
+        self.st=[]
+        self.add(root)
+    
+    def add(self,root):
+        while root:
+            self.st.append(root)
+            root=root.right
+    def nxt(self):
+        res=self.st.pop()
+        self.add(res.left)
+        return res.val
+
+
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        def solve(root):
-            nonlocal res
-            if not root or res:return
-            solve(root.left)
-            if k-root.val in d:
-                res=True
-                return
-            d.add(root.val)
-            solve(root.right)
-        d=set()
-        res=False
-        solve(root)
-        return res
-        
+        lt =LeftIter(root)
+        rt = RightIter(root)
+        left,right = lt.nxt(),rt.nxt()
+        while left<right:
+            if left+right==k:return True
+            elif left+right<k:
+                left=lt.nxt()
+            else:
+                right = rt.nxt()
+        return False
