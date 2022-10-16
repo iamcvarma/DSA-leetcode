a09456535acf7b11a4e8499a327c61ee8cc1c98f
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-10-16 15:45:02 +0530

    Time: 6221 ms (14.29%), Space: 26 MB (100.00%)

---
+++ 2440-create-components-with-same-value/2440-create-components-with-same-value.py
@@ -0,0 +1,28 @@
+class Solution:
+    def componentValue(self, nums: List[int], edges: List[List[int]]) -> int:
+        if len(nums)==1:return 0
+        def topo(target):
+            arr=nums[:]
+            adj=defaultdict(set)
+            for x,y in edges:
+                adj[x].add(y)
+                adj[y].add(x)
+            q=deque([i for i in adj if len(adj[i])==1])
+            while q:
+                curr = q.popleft()
+                if arr[curr]>target:return False
+                else:
+                    parent = list(adj[curr])[0]
+                    adj[parent].remove(curr)
+                    if arr[curr]<target:arr[parent]+=arr[curr]
+                    if len(adj[parent])==1:
+                        q.append(parent)
+                    elif len(adj[parent])==0:
+                        if arr[parent]==target:return True
+                        else:return False
+            return False
+        s=sum(nums)
+        for i in range(max(nums),s):
+            if s%i==0 and topo(i):
+                return s//i-1
+        return 0
