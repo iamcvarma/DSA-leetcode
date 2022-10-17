88c2fc9eb490a84c929c1d125488d71fa9364e56
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-10-18 01:50:16 +0530

    Time: 56 ms (66.55%), Space: 13.8 MB (98.78%)

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -3,17 +3,13 @@ class Solution:
         n  = len(s)
         res = [0]*n
         i=0
-        cc= Counter(s)
-        s = sorted(s,key = lambda x: (-cc[x],x))
-        print(s)
-        for j in range(0,n,2):
-            res[j] = s[i]
-            i+=1
-        for j in range(1,n,2):
-            res[j] = s[i]
-            i+=1
-        print(res)
-        for i in range(1,n):
-            if res[i]==res[i-1]:
-                return ""
+        cc= list(Counter(s).items())
+        cc.sort(key = lambda x:(-x[1]))
+        if cc[0][1]>((n-1)//2)+1:return ""
+        for ch,num in cc:
+            while num:
+                res[i]=ch
+                num-=1
+                i+=2
+                if i>=n:i=1
         return "".join(res)
