6a7847219e9bbf12ae7b27588820d70fbd38404a
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-10-26 11:59:11 +0530

    Time: 2628 ms (20.62%), Space: 28.9 MB (96.72%)

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        pre=[0]
-        for n in nums: pre.append(pre[-1]+n)
-        
-        hmap ={}
-        for i,v in enumerate(pre):
-            if v%k in hmap:
-                if hmap[v%k]!=i-1:
-                    return True
-            else:
-                hmap[v%k]=i
+        temp=nums[0]=nums[0]%k
+        seen=set([0])
+        for i in range(1,len(nums)):
+            nums[i]= (nums[i-1]+nums[i])%k
+            if nums[i] in seen: return True
+            seen.add(temp)
+            temp = nums[i]
         return False
