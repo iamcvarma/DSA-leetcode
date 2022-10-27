a39d87593379681da5f0973e426990ddde897fb1
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-10-27 12:07:51 +0530

    Time: 176 ms (69.14%), Space: 15.8 MB (47.72%)

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,19 +1,14 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        def process(s,a,b):
-            res=[]
-            count=0
-            for c in s:
-                if c==a:
-                    count+=1
-                    res.append(c)
-                elif c==b:
-                    if count>0:
-                        count-=1
-                        res.append(c)
-                else:
-                    res.append(c)
-            return res
-        s1 = process(s,'(',')')
-        s2 = process(s1[::-1],')','(')
-        return "".join(s2[::-1])
+        st=[]
+        s = list(s)
+        for i,c in enumerate(s):
+            if c=='(':
+                st.append(i)
+            elif c==')':
+                if st:st.pop()
+                else:s[i]=""
+        for i in st:
+            s[i]=""
+        return "".join(s)                    
+            
