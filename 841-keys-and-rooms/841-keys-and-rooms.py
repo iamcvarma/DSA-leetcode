fa8ee19884bad4d94dfcdbdba753683bdc749bd1
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-01 13:04:39 +0530

    Time: 180 ms (8.56%), Space: 15.7 MB (12.45%)

--- 841-keys-and-rooms/841-keys-and-rooms.py
+++ 841-keys-and-rooms/841-keys-and-rooms.py
@@ -1,10 +1,8 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         def dfs(curr):
-            if curr in seen:return
+            if curr in seen:return 0
             seen.add(curr)
-            for nei in rooms[curr]:
-                dfs(nei)
+            return 1+sum((dfs(j) for j in rooms[curr]),0)
         seen=set()
-        dfs(0)
-        return len(seen)==len(rooms)
+        return len(rooms)==dfs(0)
