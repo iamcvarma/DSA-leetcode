93cbd2fd12607547ec3527b248b185aeaed7ce10
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-01 12:43:49 +0530

    Time: 740 ms (97.89%), Space: 19 MB (63.23%)

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -1,10 +1,10 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        inDeg,outDeg = [0]*n,[0]*n
+        inDeg,outDeg = [0]*(n+1),[0]*(n+1)
         for x, y in trust:
-            inDeg[y-1]+=1
-            outDeg[x-1]+=1
-        for i in range(n):
+            inDeg[y]+=1
+            outDeg[x]+=1
+        for i in range(1,n+1):
             if outDeg[i]==0 and inDeg[i]==n-1:
-                return i+1
+                return i
         return -1
