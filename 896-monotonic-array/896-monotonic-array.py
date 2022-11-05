923daeac115958baddd52b74f6ba15ec7433ab63
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-05 12:37:24 +0530

    Time: 1282 ms (79.67%), Space: 28.1 MB (64.67%)

---
+++ 896-monotonic-array/896-monotonic-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        inc = dec = True
+        for i in range(1,len(nums)):
+            if nums[i-1]>nums[i]:
+                inc=False
+            if nums[i-1]<nums[i]:
+                dec=False
+        return inc or dec 
