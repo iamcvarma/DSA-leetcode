c09821ea250bf59955f86a97a04b4ae0d701c67c
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-08 13:47:26 +0530

    Time: 276 ms (34.18%), Space: 13.7 MB (98.19%)

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,14 +1,13 @@
 class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        res=0
-        carry = 0
-        for j,n in enumerate(num1):
-            carry = 0
-            temp = 0
-            for i,m in enumerate(num2[::-1]):
-                ans = int(n)*int(m)+carry
-                temp+=(ans%10)*10**i
-                carry = ans//10
-            if carry:temp+=carry*(10**(i+1))
-            res+=temp*(10**(len(num1)-j-1))
-        return str(res)
+    def multiply(self, a: str, b: str) -> str:
+        res=[0]*(len(a)+len(b))
+        a=a[::-1]
+        b=b[::-1]
+        for i,x in enumerate(a):
+            for j,y in enumerate(b):
+                res[i+j]+=int(x)*int(y)
+        for i in range(len(res)-1):
+            res[i+1]+=res[i]//10
+            res[i]%=10
+        res = "".join(map(str,res))[::-1]
+        return str(int(res))
