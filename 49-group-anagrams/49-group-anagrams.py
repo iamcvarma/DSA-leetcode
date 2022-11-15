738b33a744b6793a4d760c03a5ac155cff323719
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-15 23:21:26 +0530

    Time: 214 ms (58.05%), Space: 17.8 MB (63.98%)

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,6 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        h = defaultdict(list)
-        for s in strs:
-            h["".join(sorted(s))].append(s)
-        return h.values()
+        hmap=defaultdict(list)
+        for word in strs:
+            hmap["".join(sorted(word))].append(word)
+        return hmap.values()
