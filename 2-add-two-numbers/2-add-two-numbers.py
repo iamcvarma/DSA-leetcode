359e1f3cb13efab3051b08b2d4358344f85c551c
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-19 13:15:23 +0530

    Time: 172 ms (11.49%), Space: 14.1 MB (10.45%)

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,12 +5,20 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def solve(num1,num2,carry):
-            if not num1 and not num2:
-                if carry: return ListNode(carry)
-                return None
-            ans = (num1.val if num1 else 0) + (num2.val if num2 else 0 )+carry
-            node = ListNode(ans%10)
-            node.next = solve(num1.next if num1 else num1,num2.next if num2 else num2,ans//10)
-            return node
+        def solve(head1,head2,carry):
+            if not (head1 or head2 or carry):return None
+            val1,val2,next1,next2 = 0,0,None,None
+            if head1:
+                val1 = head1.val
+                next1 = head1.next
+            if head2:
+                val2 = head2.val
+                next2 = head2.next
+            
+            curr = val1+val2+carry
+            carry = curr//10
+            curr = curr%10
+            
+            return ListNode(curr,solve(next1,next2,carry))
         return solve(l1,l2,0)
+            
