40af4192b190af58af93134e5fe099530785dffb
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-23 13:52:47 +0530

    Time: 536 ms (32.00%), Space: 15.6 MB (6.74%)

--- 707-design-linked-list/707-design-linked-list.py
+++ 707-design-linked-list/707-design-linked-list.py
@@ -1,90 +1,54 @@
 class ListNode:
-    def __init__(self,val = 0,nxt = None,pre=None):
-        self.val = val
-        self.nxt = nxt
-        self.pre = pre
-
+    def __init__(self,val=-1,N=None,P=None):
+        self.val=val
+        self.next = N
+        self.pre = P
+        
 class MyLinkedList:
 
     def __init__(self):
-        self.head = None
-        self.length = 0
+        self.head = ListNode()
+        self.tail = ListNode()
+        self.head.next = self.tail
+        self.tail.pre = self.head
 
-    def get(self, index: int) -> int:
-        node = self.goto(index)
-        return node.val if node else -1
-        
-        
-    def goto(self,index):
-        if index>=self.length:return None
-        curr = self.head
-        for _ in range(index):
-            curr = curr.nxt
-        return curr
-    
-    
+    def get(self, i: int) -> int:
+        curr = self.head.next
+        while i and curr!=self.tail:
+            curr ,i= curr.next,i-1
+        return curr.val
+
     def addAtHead(self, val: int) -> None:
-        node = ListNode(val,self.head)
-        if self.head:
-            self.head.pre = node
-        self.head = node
-        self.length+=1
-        # self.view()
-        return None
-    
-    
-    
+        newNode = ListNode(val,self.head.next,self.head)
+        self.head.next = newNode
+        newNode.pre = self.head
+        newNode.next.pre = newNode
+
     def addAtTail(self, val: int) -> None:
-        if not self.head:
-            self.addAtHead(val)
-            return
-        curr = self.head
-        while curr.nxt:curr = curr.nxt
-        node = ListNode(val,None,curr)
-        curr.nxt = node
-        self.length+=1
-        # self.view()
-        return None
-        
-        
+        newNode = ListNode(val,self.tail,self.tail.pre)
+        self.tail.pre = newNode
+        newNode.pre.next = newNode
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index==self.length:
-            self.addAtTail(val)
-            return
-        if index==0:
-            self.addAtHead(val)
-            return
-        node = self.goto(index)
-        if not node:return
-        new_node = ListNode(val,node,node.pre)
-        if node.pre: 
-            node.pre.nxt = new_node
-        node.pre = new_node
-        self.length+=1
-        # self.view()
-    
-    
-    
+        curr = self.head.next
+        for _ in range(index):
+            curr = curr.next
+            if not curr:return
+        newNode = ListNode(val,curr,curr.pre)
+        curr.pre = newNode
+        newNode.pre.next= newNode
+        
+
     def deleteAtIndex(self, index: int) -> None:
-        node = self.goto(index)
-        if not node:return
-        if node.pre:
-            node.pre.nxt = node.nxt
-        if node.nxt:
-            node.nxt.pre = node.pre
-        if node==self.head:self.head = node.nxt
-        node.pre=node.nxt = None
-        self.length-=1
-        # self.view()
-        return
-    
-    def view(self):
-        cur = self.head
-        while cur:
-            print(cur.val)
-            cur=cur.nxt
-        print('end')
+        curr = self.head.next
+        if curr==self.tail:return
+        for _ in range(index):
+            curr = curr.next
+            if curr==self.tail:return
+        curr.next.pre = curr.pre
+        curr.pre.next = curr.next
+        
+        
 
 
 # Your MyLinkedList object will be instantiated and called as such:
