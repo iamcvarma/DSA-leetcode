3ded1ac524e59857f336e5931d6a5064763a4fef
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-24 12:38:19 +0530

    Time: 291 ms (89.95%), Space: 14.7 MB (92.81%)

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -5,10 +5,7 @@ class MyCalendar:
 
     def book(self, start: int, end: int) -> bool:
         i = bisect.bisect_right(self.arr,end-1,key = lambda x:x[0])
-        if i<1:
-            self.arr = [(start,end-1)]+self.arr
-            return True
-        pre = self.arr[i-1]
+        pre = self.arr[i-1] if i else [inf,-inf]
         if max(start,pre[0])<=min(end-1,pre[1]):return False
         self.arr = self.arr[:i]+[(start,end-1)]+self.arr[i:]
         return True
