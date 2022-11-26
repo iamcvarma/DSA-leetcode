81630151392f06e2958c8cdb882a239f13bb750b
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-26 15:27:23 +0530

    Time: 3062 ms (45.68%), Space: 13.9 MB (68.32%)

--- 611-valid-triangle-number/611-valid-triangle-number.py
+++ 611-valid-triangle-number/611-valid-triangle-number.py
@@ -2,9 +2,12 @@ class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums.sort()
         res=0
-        for i in range(len(nums)):
-            for j in range(i):
-                right=bisect.bisect_left(nums,(nums[i]+nums[j]),lo=i)
-                if right<=i:continue
-                res+=right-i-1
+        for i in range(2,len(nums)):
+            a=0
+            b=i-1
+            while a<b:
+                if nums[a]+nums[b]>nums[i]:
+                    res+=b-a
+                    b-=1
+                else:a+=1
         return res
