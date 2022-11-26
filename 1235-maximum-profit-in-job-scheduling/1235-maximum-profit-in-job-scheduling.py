7c34d392d2bf3c84da16d8d57d73836731db49d7
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-26 11:20:04 +0530

    Time: 1189 ms (65.76%), Space: 95.8 MB (11.42%)

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,17 +1,11 @@
 class Solution:
-    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+    def jobScheduling(self, start: List[int], end: List[int], profit: List[int]) -> int:
         def solve(i):
-            if i>=n: return 0
-            if i in dp: return dp[i]
-            dp[i]=max(jobs[i][2]+solve(bisect.bisect_left(jobs,jobs[i][1],key = lambda x:x[0])),solve(i+1)) 
+            if i>=n:return 0
+            if i in dp:return dp[i]
+            dp[i] = max(solve(i+1),arr[i][2]+solve(bisect.bisect_left(arr,arr[i][1],key = lambda x:x[0])))
             return dp[i]
-        
-        n = len(profit)
-        jobs=[]
         dp={}
-        for i in range(n):
-            jobs.append((startTime[i],endTime[i],profit[i]))
-        jobs.sort(key = lambda x:x[0])
-        
+        arr = sorted(zip(start,end,profit),key = lambda x:x[0])
+        n = len(arr)
         return solve(0)
-        
