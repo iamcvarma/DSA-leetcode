61c219663d27aab6141f129abd220e9f2448da7d
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-27 12:16:38 +0530

    Time: 314 ms (92.83%), Space: 15.4 MB (81.61%)

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,14 +1,11 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        j = bisect.bisect_right(arr,x)
-        i=j-1
-        while j-i-1<k:
-            left = arr[i] if i>-1 else inf
-            right = arr[j] if j<len(arr) else inf
-            if abs(left-x)<=abs(right-x):
-                i-=1
+        s,e = 0,len(arr)-k
+        while s<e:
+            m = s+e>>1
+            if x-arr[m]>arr[m+k]-x:
+                s=m+1
             else:
-                j+=1
-        return arr[i+1:j]
-                
+                e=m
+        return arr[s:s+k]
             
