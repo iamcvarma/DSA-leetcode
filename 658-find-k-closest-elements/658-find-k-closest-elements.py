8c1acdb1a96d6482b83e0a677bcd05d4694ba06c
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-27 12:02:56 +0530

    Time: 843 ms (29.53%), Space: 15.4 MB (81.61%)

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -3,11 +3,9 @@ class Solution:
         j = bisect.bisect_right(arr,x)
         i=j-1
         while j-i-1<k:
-            left = arr[i] if i>-1 else -inf
+            left = arr[i] if i>-1 else inf
             right = arr[j] if j<len(arr) else inf
-            if abs(left-x)<abs(right-x):
-                i-=1
-            elif abs(left-x)==abs(right-x):
+            if abs(left-x)<=abs(right-x):
                 i-=1
             else:
                 j+=1
