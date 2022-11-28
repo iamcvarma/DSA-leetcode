a8b4174394fd39d8fa08227d4fc54a5248b6aa0e
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-28 12:51:11 +0530

    Time: 103 ms (92.58%), Space: 14.3 MB (45.48%)

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,9 +1,18 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        def bsearch(target):
+            s,e = 0,len(nums)-1
+            while s<e:
+                m = s+e>>1
+                if dp[m]>=target:
+                    e=m
+                else:
+                    s=m+1
+            return e
         dp = [inf]*len(nums)
-        ma = 1
+        ma = 0
         for n in nums:
             i = bisect.bisect_left(dp,n)
             dp[i] = min(dp[i],n)
-            ma = max(ma,i+1)
-        return ma
+            ma = max(ma,i)
+        return ma+1
