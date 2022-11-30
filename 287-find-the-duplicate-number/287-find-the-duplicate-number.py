8dacff731bc2e227b3bfb1b1d887d8a38d14a98a
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-11-30 13:38:36 +0530

    Time: 655 ms (92.75%), Space: 27.9 MB (59.93%)

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,13 +1,11 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        #bsearch
-        def check(m):
-            return sum(i<=m for i in nums)<=m
-        s,e = 1,len(nums)-1
-        while s<e:
-            m = s+e>>1
-            if check(m):
-                s=m+1
-            else:
-                e=m
+        s,f = nums[0],nums[nums[0]]
+        while s!=f:
+            s = nums[s]
+            f = nums[nums[f]]
+        f = 0
+        while s!=f:
+            s = nums[s]
+            f = nums[f]
         return s
