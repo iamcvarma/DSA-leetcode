fda97cb7dfefb4d20642bb54250e45aa840f6b86
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-12-02 13:29:32 +0530

    Time: 523 ms (28.80%), Space: 15.2 MB (80.00%)

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -0,0 +1,5 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        a= Counter(word1)
+        b = Counter(word2)
+        return sorted(a.keys())==sorted(b.keys()) and sorted(a.values())==sorted(b.values())
