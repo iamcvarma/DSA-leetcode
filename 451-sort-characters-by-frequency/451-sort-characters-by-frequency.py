bb7971e3946c4e38d956ceeb0e75767051e63fbb
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-12-03 13:13:39 +0530

    Time: 250 ms (5.07%), Space: 28.4 MB (5.07%)

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -1,12 +1,4 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        freq = Counter(s)
-        bucket = [""]*(len(s)+1)
-        for c,f in freq.items():
-            bucket[f]+=c
-        res=[]
-        for i in range(len(s),0,-1):
-            if bucket[i]:
-                for c in bucket[i]:
-                    res.append(c*i)
-        return ''.join(res)
+        cc =Counter(s)
+        return "".join(sorted(s,key = lambda x:(-cc[x],x)))
