fa8b731565f5682b20d8839776e8ad6e4e95dd45
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2022-12-03 13:19:51 +0530

    Time: 54 ms (86.58%), Space: 17.3 MB (19.98%)

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -1,4 +1,8 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         cc =Counter(s)
-        return "".join(sorted(s,key = lambda x:(-cc[x],x)))
+        arr = sorted(cc.keys(),key = lambda x:(-cc[x],x))
+        res=[]
+        for c in arr:
+            res.extend([c]*cc[c])
+        return res
