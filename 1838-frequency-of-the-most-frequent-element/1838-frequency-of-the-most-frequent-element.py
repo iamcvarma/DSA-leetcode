c1163e64a581fe8d209041e26b607ad7a72a8fc2
iamcvarma <102286762+iamcvarma@users.noreply.github.com>
2023-11-18 22:45:28 +0530

    Time: 1176 ms (45.86%), Space: 31.3 MB (18.27%)

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -1,11 +1,14 @@
 class Solution:
-    def maxFrequency(self, nums: List[int], curr: int) -> int:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
         nums.sort()
-        ma=i=0
+        i = 0
+        curr = 0
+        ma = 0
         for j in range(len(nums)):
             curr+=nums[j]
-            while curr<nums[j]*(j-i+1):
+            while nums[j]*(j-i+1)-curr>k:
                 curr-=nums[i]
                 i+=1
-            ma = max(ma,j-i+1)
+            ma = max(ma, j-i+1)
         return ma
+        
